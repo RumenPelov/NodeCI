04661c1652a9bfcf4cedc3d11b31e95a223b6dcd
Rumen Pelov <rumenpelov@yahoo.com>
2018-06-16 16:07:59 -0400

    Remove dev

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://RP:password1@ds239930.mlab.com:39930/blog_dev',
-  cookieKey: '123123123',
-  redisUrl : 'redis://127.0.0.1:6379',
-  headless: false
-};
